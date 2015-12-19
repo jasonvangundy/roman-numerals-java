b3aeb2396b94861be2bc863dde0eea1d98b95b2f
Jason <jason.vangundy@gmail.com>
2015-12-18 23:43:53 -0600

    refactor out dup new statements

--- src/test/java/katas/RomanNumeralsToArabicTest.java
+++ src/test/java/katas/RomanNumeralsToArabicTest.java
@@ -1,89 +1,97 @@
 package katas;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class RomanNumeralsToArabicTest {
 
+    private RomanNumerals romanNumerals;
+
+    @Before
+    public void setUp() throws Exception {
+        romanNumerals = new RomanNumerals();
+    }
+
     @Test
     public void testTranslatesSingleI() throws Exception {
-        assertEquals(1, new RomanNumerals().romanNumeralToArabic("I"));
+        assertEquals(1, romanNumerals.romanNumeralToArabic("I"));
     }
 
     @Test
     public void testTranslatesMultipleIs() throws Exception {
-        assertEquals(3, new RomanNumerals().romanNumeralToArabic("III"));
+        assertEquals(3, romanNumerals.romanNumeralToArabic("III"));
     }
 
     @Test
     public void testTranslatesV() throws Exception {
-        assertEquals(5, new RomanNumerals().romanNumeralToArabic("V"));
+        assertEquals(5, romanNumerals.romanNumeralToArabic("V"));
     }
 
     @Test
     public void testTranslatesVAndICombination() throws Exception {
-        assertEquals(8, new RomanNumerals().romanNumeralToArabic("VIII"));
+        assertEquals(8, romanNumerals.romanNumeralToArabic("VIII"));
     }
 
     @Test
     public void testTranslatesIV() throws Exception {
-        assertEquals(4, new RomanNumerals().romanNumeralToArabic("IV"));
+        assertEquals(4, romanNumerals.romanNumeralToArabic("IV"));
     }
 
     @Test
     public void testTranslatesX() throws Exception {
-        assertEquals(10, new RomanNumerals().romanNumeralToArabic("X"));
+        assertEquals(10, romanNumerals.romanNumeralToArabic("X"));
     }
 
     @Test
     public void testTranslatesIX() throws Exception {
-        assertEquals(9, new RomanNumerals().romanNumeralToArabic("IX"));
+        assertEquals(9, romanNumerals.romanNumeralToArabic("IX"));
     }
 
     @Test
     public void testTranslatesL() throws Exception {
-        assertEquals(52, new RomanNumerals().romanNumeralToArabic("LII"));
+        assertEquals(52, romanNumerals.romanNumeralToArabic("LII"));
     }
 
     @Test
     public void testTranslatesXL() throws Exception {
-        assertEquals(42, new RomanNumerals().romanNumeralToArabic("XLII"));
+        assertEquals(42, romanNumerals.romanNumeralToArabic("XLII"));
     }
 
     @Test
     public void testTranslatesC() throws Exception {
-        assertEquals(102, new RomanNumerals().romanNumeralToArabic("CII"));
+        assertEquals(102, romanNumerals.romanNumeralToArabic("CII"));
     }
 
     @Test
     public void testTranslatesXC() throws Exception {
-        assertEquals(92, new RomanNumerals().romanNumeralToArabic("XCII"));
+        assertEquals(92, romanNumerals.romanNumeralToArabic("XCII"));
     }
 
     @Test
     public void testTranslatesD() throws Exception {
-        assertEquals(502, new RomanNumerals().romanNumeralToArabic("DII"));
+        assertEquals(502, romanNumerals.romanNumeralToArabic("DII"));
     }
 
     @Test
     public void testTranslatesCD() throws Exception {
-        assertEquals(402, new RomanNumerals().romanNumeralToArabic("CDII"));
+        assertEquals(402, romanNumerals.romanNumeralToArabic("CDII"));
     }
 
     @Test
     public void testTranslatesM() throws Exception {
-        assertEquals(1002, new RomanNumerals().romanNumeralToArabic("MII"));
+        assertEquals(1002, romanNumerals.romanNumeralToArabic("MII"));
     }
 
     @Test
     public void testTranslatesCM() throws Exception {
-        assertEquals(902, new RomanNumerals().romanNumeralToArabic("CMII"));
+        assertEquals(902, romanNumerals.romanNumeralToArabic("CMII"));
     }
 
     @Test
     public void testAdditionalKataProvidedTestCases() throws Exception {
-        assertEquals(1066, new RomanNumerals().romanNumeralToArabic("MLXVI"));
-        assertEquals(1989, new RomanNumerals().romanNumeralToArabic("MCMLXXXIX"));
+        assertEquals(1066, romanNumerals.romanNumeralToArabic("MLXVI"));
+        assertEquals(1989, romanNumerals.romanNumeralToArabic("MCMLXXXIX"));
     }
 }
